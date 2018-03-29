d95296582ff63d587bfd3bd0a727b8d2cacaeaf1
Diraugo <37903806+Diraugo@users.noreply.github.com>
2018-03-29 21:21:17 +0200

    Beginn Spielobjekte
    grundgerüst einlesen und anzeige der objekte
    außerdem platzhalter texturen hinzugefügt

--- Game1.cs
+++ Game1.cs
@@ -1,16 +1,75 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 
 namespace schiebespiel
 {
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
+    /// 
+    public class Spielobjekte
+    {
+        private int x;
+        private int y;
+
+        private bool zeichneBoden;
+
+        Texture2D Textur;
+
+
+        public Spielobjekte(int pX,int pY, Texture2D pTextur,bool pZeichne)
+        {
+            this.x = pX;
+            this.y = pY;
+            Textur = pTextur;
+            this.zeichneBoden = pZeichne;
+        }
+
+        public int getX()
+        {
+            return this.x;
+        }
+        public int getY()
+        {
+            return this.y;
+        }
+        public bool getZeichneBoden()
+        {
+            return this.zeichneBoden;
+        }
+        public Texture2D getTextur()
+        {
+            return this.Textur;
+        }
+        public void setPosition(int pX,int pY)
+        {
+            this.x = pX;
+            this.y = pY;
+        }
+    }
+
+
+
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
+        Texture2D TexturTisch;
+        Texture2D TexturWand;
+        Texture2D TexturBox;
+        Texture2D TexturSpieler;
+        Texture2D TexturBoden;
+
+        List<Spielobjekte> lSpielobjekte=new List<Spielobjekte>();
+
+        SpriteFont Anzeige;
+
+        String Spielfeld;
+        string[] SpielArray;
         
         public Game1()
         {
@@ -27,6 +86,14 @@ namespace schiebespiel
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            Spielfeld = "wwwwwww\n" +
+                "w**p**w\n" +
+                "w**t**w\n" +
+                "w**x**w\n" +
+                "wwwwwww";
+            SpielArray=Spielfeld.Split('\n');
+            
+            
 
             base.Initialize();
         }
@@ -41,6 +108,43 @@ namespace schiebespiel
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            Anzeige = Content.Load<SpriteFont>("Text");
+            TexturBox = Content.Load<Texture2D>("box");
+            TexturBoden = Content.Load<Texture2D>("boden");
+            TexturWand = Content.Load<Texture2D>("wand");
+            TexturSpieler = Content.Load<Texture2D>("spieler");
+            TexturTisch = Content.Load<Texture2D>("tisch");
+
+            Spielobjekte tmpObjekt;
+            for (int i = 0; i < SpielArray.Length; i++)
+            {
+                for (int j = 0; j < SpielArray[i].Length; j++)
+                {
+                    switch (SpielArray[i].ToCharArray()[j])
+                    {
+                        case 'w':
+                            tmpObjekt = new Spielobjekte(j, i, TexturWand,false);
+                            break;
+                        case '*':
+                            tmpObjekt = new Spielobjekte(j, i, TexturBoden,false);
+                            break;
+                        case 'p':
+                            tmpObjekt = new Spielobjekte(j,i, TexturSpieler,true);
+                            break;
+                        case 'x':
+                            tmpObjekt = new Spielobjekte(j, i, TexturBox,true);
+                            break;
+                        case 't':
+                            tmpObjekt = new Spielobjekte(j, i, TexturTisch, true);
+                            break;
+                        default:
+                            tmpObjekt = new Spielobjekte(j, i, TexturBoden,false);
+                            break;
+                    }
+
+                    lSpielobjekte.Add(tmpObjekt);
+                }
+            }
         }
 
         /// <summary>
@@ -76,7 +180,17 @@ namespace schiebespiel
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-
+            spriteBatch.Begin();
+            for(int i=0;i<lSpielobjekte.Count;i++)
+            {
+                if(lSpielobjekte[i].getZeichneBoden())
+                {
+                    spriteBatch.Draw(TexturBoden, new Rectangle(lSpielobjekte[i].getX() * 80, lSpielobjekte[i].getY() * 80, 80, 80), Color.White);
+                }
+                spriteBatch.Draw(lSpielobjekte[i].getTextur(),new Rectangle(lSpielobjekte[i].getX()*80,lSpielobjekte[i].getY()*80,80,80),Color.White);
+            }
+            //spriteBatch.DrawString(Anzeige, Spielfeld, new Vector2(50, 50), Color.White);
+            spriteBatch.End();
             base.Draw(gameTime);
         }
     }
